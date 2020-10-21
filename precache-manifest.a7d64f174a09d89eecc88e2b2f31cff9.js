57c392b0731227fcbc0011554fddded577c11a38
Jalpit Patel <jalpit.patel@gatewaygroup.com>
2020-10-21 19:17:01 +0530

    Updates

---
+++ precache-manifest.a7d64f174a09d89eecc88e2b2f31cff9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8392bbbafdc26a5af6b40729a564ea",
+    "url": "/ToDoReactDemo/index.html"
+  },
+  {
+    "revision": "7284794bf85d72866329",
+    "url": "/ToDoReactDemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5810472bc2ef7c42ed16",
+    "url": "/ToDoReactDemo/static/js/2.b2897631.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/ToDoReactDemo/static/js/2.b2897631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7284794bf85d72866329",
+    "url": "/ToDoReactDemo/static/js/main.d7f3f9f4.chunk.js"
+  },
+  {
+    "revision": "a1614ee5f03bbdc05601",
+    "url": "/ToDoReactDemo/static/js/runtime-main.963f4459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoReactDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
